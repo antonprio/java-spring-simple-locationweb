c298edfc9a7b020ba3bdbaf628cfb6463417594c
Anton Prio Hutomo <antonpriohutomo@gmail.com>
2020-04-09 22:17:43 +0700

    Finished API for Location

--- src/main/java/com/antonprio/location/controllers/LocationRESTController.java
+++ src/main/java/com/antonprio/location/controllers/LocationRESTController.java
@@ -3,11 +3,10 @@ package com.antonprio.location.controllers;
 import com.antonprio.location.entities.Location;
 import com.antonprio.location.repos.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/locations")
@@ -19,5 +18,25 @@ public class LocationRESTController {
     @GetMapping
     public List<Location> getLocations() {
         return locationRepository.findAll();
+    }
+
+    @PostMapping
+    public Location createLocation(@RequestBody Location location) {
+        return locationRepository.save(location);
+    }
+
+    @PutMapping
+    public Location updateLocation(@RequestBody Location location) {
+        return locationRepository.save(location);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLocation(@PathVariable("id") int id) {
+        locationRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Location> getLocation(@PathVariable("id") int id) {
+        return locationRepository.findById(id);
     }
 }
